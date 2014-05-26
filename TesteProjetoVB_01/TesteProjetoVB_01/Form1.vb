c65261a5d38fb3852a895d7c1c2b5c56f092c73d
malopes21 <malopes21@gmail.com>
2014-05-26 21:47:26 +0000

    teste02

--- TesteProjetoVB_01/TesteProjetoVB_01/Form1.vb
+++ TesteProjetoVB_01/TesteProjetoVB_01/Form1.vb
@@ -3,5 +3,7 @@
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         MessageBox.Show("Olá mundo novo!")
+
+
     End Sub
 End Class
